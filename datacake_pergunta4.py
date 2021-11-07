6b6d2bdf0437fad33f21eafa47e6e50aeb155f69
Jefferson Luiz Seno <65913458+JSeno@users.noreply.github.com>
2021-11-07 08:45:56 -0300

    Alterado nome do arquivo
    Foi alterado nome do arquivo para pergunta 4 correspondente ao teste.

--- datacake_pergunta4.py
+++ datacake_pergunta4.py
@@ -2,6 +2,7 @@ lista1 = [9, 2, 3, 1, 4]
 res = [i for i in range(max(lista1)+1) if i not in lista1]
 list2 = res + lista1
 list2.sort()
+
 print(f'Encontre os números faltando nessa lista: {lista1}')
 print(f'Números faltantes na lista: {res}')
 print(f'Lista organizada: {list2}')
